01ab0cdb52e5886ee9185bafed64b1d55aadd645
paracap <117801866+swapnil15021997@users.noreply.github.com>
2025-09-13 17:05:47 +0530

    inital commit set up all
    inital commit set up all

---
+++ auth/auth_app/views.py
@@ -0,0 +1,27 @@
+from django.shortcuts import render
+from django.contrib.auth import authenticate
+from django.contrib.auth.forms import UserCreationForm,AuthenticationForm
+from django.contrib.auth.decorators import login_required
+
+def register_view(request):
+    if request.method == "POST":
+        pass
+    
+    else:
+        pass
+    
+    
+
+
+
+def login_view(request):
+    pass
+
+
+def home():
+    pass
+
+
+def logout():
+    pass
+# Create your views here.
